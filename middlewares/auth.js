879a9b9f95c7623d18bc865bec65e7704ae979c8
TatianaMil <ftv9988.it@gmail.com>
2023-05-16 00:11:16 +0400

    add rerurn

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,14 +7,14 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new AuthError('Необходима авторизация'));
+    return next(new AuthError('Необходима авторизация'));
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    next(new AuthError('Необходима авторизация_2'));
+    return next(new AuthError('Необходима авторизация_2'));
   }
   req.user = payload;
   return next();
